6db40b90df4c5ebbb54ce6bf5adbcfece2bcac7d
RogerWitkowski <witkowski.roger@gmail.com>
2022-12-12 16:52:36 +0100

    update dishDto model with id prop

--- Restaurant.Models/Dto/DishDto.cs
+++ Restaurant.Models/Dto/DishDto.cs
@@ -8,6 +8,7 @@ namespace Restaurant.Models.Dto
 {
     public class DishDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
