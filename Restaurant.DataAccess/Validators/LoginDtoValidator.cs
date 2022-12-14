e674637a3dae7529f02e6344e0250e3250db204f
RogerWitkowski <witkowski.roger@gmail.com>
2022-12-14 13:16:58 +0100

    LoginDto Validator implemented

---
+++ Restaurant.DataAccess/Validators/LoginDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Restaurant.DataAccess.DataAccess;
+using Restaurant.Models.Dto;
+
+namespace Restaurant.DataAccess.Validators
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator(RestaurantDbContext dbContext)
+        {
+            RuleFor(e => e.Email).NotEmpty().EmailAddress();
+            RuleFor(p => p.Password).MinimumLength(10).NotEmpty();
+        }
+    }
+}
