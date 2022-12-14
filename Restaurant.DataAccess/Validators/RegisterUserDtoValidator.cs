0074012987d38b4ead0ff1f5b991e71d1a76eac0
RogerWitkowski <witkowski.roger@gmail.com>
2022-12-14 11:32:52 +0100

    validator for registerUserDto implemented and registered

---
+++ Restaurant.DataAccess/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.DataAccess.DataAccess;
+using Restaurant.Models.Dto;
+
+namespace Restaurant.DataAccess.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(RestaurantDbContext dbContext)
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.AnyAsync(u => u.Email == value);
+                    if (emailInUse.Result)
+                    {
+                        context.AddFailure("Email", "That email is taken!");
+                    }
+                });
+
+            RuleFor(p => p.Password)
+                .MinimumLength(10);
+
+            RuleFor(cp => cp.ConfirmPassword)
+                .Equal(p => p.Password);
+        }
+    }
+}
