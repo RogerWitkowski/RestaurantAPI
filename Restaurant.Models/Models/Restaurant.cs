e353c46fba0eeb2c08e0d735bea4a33502f9caa1
RogerWitkowski <witkowski.roger@gmail.com>
2022-12-14 22:52:32 +0100

    updated restaurant model with prop createdByUserId

--- Restaurant.Models/Models/Restaurant.cs
+++ Restaurant.Models/Models/Restaurant.cs
@@ -18,6 +18,8 @@ namespace Restaurant.Models.Models
         public bool HasDelivery { get; set; }
         public string ContactEmail { get; set; }
         public string ContactNumber { get; set; }
+        public int? CreatedById { get; set; }
+        public virtual User CreatedBy { get; set; }
         public int AddressId { get; set; }
         public virtual Address Address { get; set; }
         public virtual List<Dish> Dishes { get; set; }
